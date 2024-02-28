c90a879258079de8e36c629c39a3f77ecb99a016
poravoz <116353344+poravoz@users.noreply.github.com>
2024-02-28 22:26:59 +0200

    28.02.2024

---
+++ frontend/src/App.tsx
@@ -0,0 +1,9 @@
+import HelloWorld from './Components/HelloWorldComponent/helloWorld';
+
+function App() {
+  return (
+    <HelloWorld></HelloWorld>
+  );
+}
+
+export default App;
---
+++ frontend/src/Components/HelloWorldComponent/helloWorld.tsx
@@ -0,0 +1,10 @@
+
+
+function HelloWorld() {
+    return (
+      <div> Hello, World ! </div>
+    );
+  }
+  
+  export default HelloWorld;
+  
