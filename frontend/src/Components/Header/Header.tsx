6e6bc15cb9bd178f20a459b8e7e7eef676458747
poravoz <116353344+poravoz@users.noreply.github.com>
2024-03-01 15:30:35 +0200

    01.03.2024

--- frontend/src/Components/Header/Header.tsx
+++ frontend/src/Components/Header/Header.tsx
@@ -1,9 +1,17 @@
 import './assets/css/Header.css'
 
+import miniRobot from './assets/images/miniRobot.png'
+
 function Header() {
     return (
         <div className="container_header">
+            <h2 className="text_header"> BrainStorm - some content </h2>
+            <h4 className="text_subtitle"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod <br></br> 
+                 tempor incididunt ut labore et dolore magna aliqua. </h4>
             
+            <button className= "button_read_more_header"> Read More </button>
+
+            <img className='miniRobot_header' src={miniRobot} />
         </div>
     )
 }
