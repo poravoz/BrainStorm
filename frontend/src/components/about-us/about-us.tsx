edf5850516e9462f906a687dd21efc36720761c0
poravoz <116353344+poravoz@users.noreply.github.com>
2024-03-02 18:46:52 +0200

    02.03.2024

--- frontend/src/components/about-us/about-us.tsx
+++ frontend/src/components/about-us/about-us.tsx
@@ -3,7 +3,7 @@ import './about-us.css'
 const AboutUs = () => { 
     return(
         <div className='container_about_us'>
-
+            <p className='text_about_us'> About us</p>
         </div>
     )
 }
