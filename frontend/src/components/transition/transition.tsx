03163f34d7a5bcb57985d50fdff76c3261b48197
poravoz <116353344+poravoz@users.noreply.github.com>
2024-03-04 14:14:02 +0200

    04.03.2024

--- frontend/src/components/transition/transition.tsx
+++ frontend/src/components/transition/transition.tsx
@@ -1,10 +1,16 @@
-import background from "./images/background.jpg"
 import './transition.css'
+import robot from './images/robot.png'
+import google_play_button from './images/google-play-badge.png'
 
 const Transition = () => { 
     return(
        <div className="container_transition">
+            <img className="robot_transition" src={robot} />
+            <p className="text_transition"> Learn to code anytime, <br /> from anywhere </p>
             
+            <a className='google_play_button_transition_a' href="">
+                <img className="google_play_button_transition" src={google_play_button} /> 
+            </a>
        </div>
     ) 
 }
