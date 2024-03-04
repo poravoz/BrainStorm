17e51a53aa8f0ac079fced2a27ad08b9257125ef
poravoz <116353344+poravoz@users.noreply.github.com>
2024-03-05 01:42:55 +0200

    05.03.2024

--- frontend/src/components/our-team/our-team.tsx
+++ frontend/src/components/our-team/our-team.tsx
@@ -13,11 +13,11 @@ const OurTeam = () => {
       <p className="text_our_team"> Our team </p>
       <Slider {...settings}>
         {data.map((item) => (
-          <div>
-            <img className="img_our_team" src={item.img} />
-            <h3 className="name_our_team">{item.name}</h3>
-            <h4 className="review_our_team">{item.review}</h4>
-          </div>
+            <>
+              <img className="img_our_team" src={item.img} />
+              <h3 className="name_our_team">{item.name}</h3>
+              <h4 className="review_our_team">{item.review}</h4>
+            </>
         ))}
       </Slider>
     </div>
