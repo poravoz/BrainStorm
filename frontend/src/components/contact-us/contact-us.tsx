322754650ac2a1e2bebf3374316fa9ee3191a468
poravoz <116353344+poravoz@users.noreply.github.com>
2024-03-06 21:18:46 +0200

    06.03.2024

--- frontend/src/components/contact-us/contact-us.tsx
+++ frontend/src/components/contact-us/contact-us.tsx
@@ -1,9 +1,21 @@
 import "./contact-us.css"
+import robotSendMessage from "./images/robotSendMessage.png"
 
 const ContactUs = () => { 
     return (
-        <div className="container_contactUs"> 
-            
+        <div className="container_contactUs">
+            <p className="text_contactUs"> Contact us </p> 
+            <form className="form_contactUs">
+                <p className="name_contactUs">Name:</p>
+                <input placeholder="Your name" className="input_contactUs" type="text" /> <br />
+                <p className="email_contactUs">Email:</p>
+                <input placeholder="Your email" className="input_contactUs" type="email"/> <br />
+                <p className="message_contactUs"> Your message: </p>
+                <textarea className="textarea_contact_us" placeholder="Enter Something"></textarea> <br />
+                <button className="button_contactUs" > Submit </button>
+                <img className="robotSendMessage_img" src={robotSendMessage} />
+            </form>
+
         </div>
     )
 }
