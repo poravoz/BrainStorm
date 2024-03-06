5dd7180bdd1b903f0a2913a5d99c01102db6083f
poravoz <116353344+poravoz@users.noreply.github.com>
2024-03-06 22:37:50 +0200

    06.03.2024

--- frontend/src/components/contact-us/contact-us.tsx
+++ frontend/src/components/contact-us/contact-us.tsx
@@ -1,14 +1,18 @@
 import "./contact-us.css"
 import robotSendMessage from "./images/robotSendMessage.png"
+import { MdEmail } from "react-icons/md";
+import { GrAccessibility } from "react-icons/gr";
 
 const ContactUs = () => { 
     return (
         <div className="container_contactUs">
             <p className="text_contactUs"> Contact us </p> 
             <form className="form_contactUs">
+                <GrAccessibility size={25} className="mdName_contact_us" color='#05386b' />
                 <p className="name_contactUs">Name:</p>
                 <input placeholder="Your name" className="input_contactUs" type="text" /> <br />
                 <p className="email_contactUs">Email:</p>
+                <MdEmail size={25} className='mdEmail_contact_us' color='#05386b' />
                 <input placeholder="Your email" className="input_contactUs" type="email"/> <br />
                 <p className="message_contactUs"> Your message: </p>
                 <textarea className="textarea_contact_us" placeholder="Enter Something"></textarea> <br />
