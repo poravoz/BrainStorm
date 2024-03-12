db51e96611f2efcbc1c6a10e3a7196aa18558424
poravoz <116353344+poravoz@users.noreply.github.com>
2024-03-12 23:07:25 +0200

    12.03.2024

--- frontend/src/components/main-page/about-us/about-us.tsx
+++ frontend/src/components/main-page/about-us/about-us.tsx
@@ -1,48 +1,37 @@
 import './about-us.css'
 import {useContext } from 'react';
 import { ThemeContext } from '../../../contexts/theme';
+import { useTranslation } from 'react-i18next';
 
 const AboutUs = () => { 
     const [{ theme }] = useContext(ThemeContext);
-
+    const [t] = useTranslation("global");
     return(
         <div className="container_about_us" style={{backgroundColor: theme. backgroundColor_about_us}}>
-             <p className='text_about_us' style={{color: theme.color_about_us}}> About us </p>
+             <p className='text_about_us' style={{color: theme.color_about_us}}> {t("about-us.about-us")} </p>
             <div className="item_about_us">
                 <div className="layer_about_us" style={{backgroundColor: theme.backgroundColor_about_us_card, border: theme.border_about_us_card}}>
-                    <h3 className="h3_about_us" style={{color: theme.color_about_us_card}}> Creators of the Future </h3>
+                    <h3 className="h3_about_us" style={{color: theme.color_about_us_card}}> {t("about-us.title-card1")} </h3>
                         <p className="p_about_us" style={{color: theme.color_about_us_card}}>
-                        Go on a journey into the world of programming 
-                        with us! We are a team of inventors creating 
-                        a unique application for learning programming. 
-                        Our mission is not just to teach you how to code, 
-                        but to open up to you the limitless possibilities 
-                        of the technological future.
+                            {t("about-us.text-card1")}
                         </p>
                 </div>
             </div>
 
             <div className="item_about_us">
                 <div className="layer_about_us" style={{backgroundColor: theme.backgroundColor_about_us_card, border: theme.border_about_us_card}}>
-                    <h3 className="h3_about_us" style={{color: theme.color_about_us_card}}> Our Direction </h3>
+                    <h3 className="h3_about_us" style={{color: theme.color_about_us_card}}> {t("about-us.title-card2")} </h3>
                         <p className="p_about_us" style={{color: theme.color_about_us_card}}>   
-                            We are not just an app - we are here to improve you. 
-                            With our experience and passion for code, we give you 
-                            the tools to learn programming successfully. 
-                            Overcoming challenges is easy when you choose our way.
+                            {t("about-us.text-card2")}
                         </p>
                 </div>
             </div>
 
             <div className="item_about_us">
                 <div className="layer_about_us" style={{backgroundColor: theme.backgroundColor_about_us_card, border: theme.border_about_us_card}}>
-                    <h3 className="h3_about_us" style={{color: theme.color_about_us_card}}> Learning is Easy </h3>
+                    <h3 className="h3_about_us" style={{color: theme.color_about_us_card}}> {t("about-us.title-card3")} </h3>
                         <p className="p_about_us" style={{color: theme.color_about_us_card}}>
-                            For us, every line of code is a step into the future. 
-                            We are enthusiasts who create an educational application 
-                            where each of you can discover the world of programming. 
-                            Join us and let's change the world together with every 
-                            block of code we write
+                            {t("about-us.text-card3")}
                         </p>
                 </div>
             </div>
