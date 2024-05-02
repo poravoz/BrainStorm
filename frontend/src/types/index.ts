ddbd03af6a6d57c5a572a60a3c84c880c0e7935c
Zatiria <yana02trofimenko@gmail.com>
2024-05-02 13:22:51 +0300

    Fix + cleaning

--- frontend/src/types/index.ts
+++ frontend/src/types/index.ts
@@ -1,22 +1,3 @@
-export type ProductItem = {
-    title: string,
-    category: string,
-    price: number,
-    image: string;
-}
-  
-export interface ShoppingCartLine {
-    product: ProductItem;
-    count: number;
-}
-  
-export interface AppState {
-    catalog: ProductItem[];
-    shoppingCart: ShoppingCartLine[];
-    totalPrice: number;
-}
-
-
 export interface Product {
     id: number;
     category: string;
